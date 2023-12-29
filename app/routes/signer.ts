a46b89829f9720c7b935acb5ab183c438267a0e9
codespool <4625220+codespool@users.noreply.github.com>
2023-12-29 19:51:26 +0200

    add signer api

---
+++ app/routes/signer.ts
@@ -0,0 +1,51 @@
+import { json, type ActionFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
+import { createWalletClient, encodePacked, http, keccak256, publicActions, toBytes } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { astarZkatana } from "viem/chains";
+import { Buffer } from "buffer";
+
+const RPC = process.env.RPC;
+const WALLET_KEY = process.env.WALLET_KEY;
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  switch (request.method) {
+    case "POST": {
+      const body = await request.json();
+      const signature = await signMessage(body);
+      return json(signature);
+    }
+  }
+};
+
+async function signMessage({ to, tokenId, quantity, contractAddress }: ContractDetails) {
+  const transport = http(RPC);
+
+  if (!WALLET_KEY) throw new Error("WALLET_KEY is not set");
+  const account = privateKeyToAccount(`0x${WALLET_KEY}`);
+
+  const client = createWalletClient({
+    account,
+    chain: astarZkatana,
+    transport,
+  }).extend(publicActions);
+
+  const nonce = await client.getTransactionCount({ address: account.address });
+
+  const encodedMessage = encodePacked(
+    ["address", "uint256", "uint256", "uint256", "address"],
+    [to, BigInt(tokenId), BigInt(quantity), BigInt(nonce), contractAddress],
+  );
+  const hash = keccak256(encodedMessage);
+  const hashBytes = toBytes(hash);
+
+  const signedMessage = await client.signMessage({ account, message: { raw: hashBytes } });
+
+  return { signedMessage, hashBytes, nonce };
+}
+
+type ContractDetails = {
+  to: `0x${string}`;
+  tokenId: number;
+  quantity: number;
+  contractAddress: `0x${string}`;
+};
